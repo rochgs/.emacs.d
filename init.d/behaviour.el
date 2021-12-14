8d0728b4f0a81dfe015dd318b44f470fce2e92f3
Rodrigo García Suárez <roch@bebanjo.com>
2021-12-14 18:08:26 +0100

    emacs: syncs exec path with user's PATH

--- init.d/behaviour.el
+++ init.d/behaviour.el
@@ -14,27 +14,43 @@
   (interactive)
   (save-excursion
     (if (region-active-p)
-	(progn
-	  (indent-region (region-beginning) (region-end))
-	  (whitespace-cleanup-region (region-beginning) (region-end))
-	  (message "Indented selected region."))
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (whitespace-cleanup-region (region-beginning) (region-end))
+          (message "Indented selected region."))
       (progn
-	(indent-buffer)
-	(whitespace-cleanup)
-	(message "Indented buffer.")))))
+        (indent-buffer)
+        (whitespace-cleanup)
+        (message "Indented buffer.")))))
 (global-set-key (kbd "C-c i") 'indent-region-or-buffer)
 ;; Rename file & buffer
 (defun rename-file-and-buffer (new-name)
   "Renames both current buffer and file it's visiting to NEW-NAME."
   (interactive "sNew name: ")
   (let ((name (buffer-name))
-	(filename (buffer-file-name)))
+        (filename (buffer-file-name)))
     (if (not filename)
-	(message "Buffer '%s' is not visiting a file!" name)
+        (message "Buffer '%s' is not visiting a file!" name)
       (if (get-buffer new-name)
-	  (message "A buffer named '%s' already exists!" new-name)
-	(progn
-	  (rename-file filename new-name 1)
-	  (rename-buffer new-name)
-	  (set-visited-file-name new-name)
-	  (set-buffer-modified-p nil))))))
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+;; Required by GUI when started outside a shell:
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$" "" (shell-command-to-string
+                                          "$SHELL --login -c 'echo $PATH'"
+                                          ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
