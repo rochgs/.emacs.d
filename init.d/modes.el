24083c84f49f59d29474519b0f998421193c0d29
Rodrigo García Suárez <roch@bebanjo.com>
2022-02-08 17:03:32 +0100

    enable smartparens-mode by default for markdown files

--- init.d/modes.el
+++ init.d/modes.el
@@ -25,6 +25,7 @@
 (require 'smartparens-config)
 (add-hook 'enh-ruby-mode-hook 'smartparens-mode)
 (add-hook 'org-mode-hook 'smartparens-mode)
+(add-hook 'markdown-mode-hook 'smartparens-mode)
 ;; web-mode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
